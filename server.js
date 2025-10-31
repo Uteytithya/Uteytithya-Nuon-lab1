0452c689de88d9d28449b76ef21812885b6cc56d
Uteytithya <tithyautey@gmail.com>
2025-10-31 10:56:28 +0700

    Test Deployment Stage Second!

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const app = require('./app');
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`App listening at http://localhost:${PORT}`);
+  console.log(`App listening at http://localhost:${PORT}!`);
 });
